fb60f0dda414f5ad7afe335b0263bbed7cf93cf7
Ramil Garipov <garipovramil93@hotmail.ca>
2021-10-29 17:59:17 -0700

    added firebase authentication in login.html and the javascript in firebaseAPI.js.

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCGt9cdC_FV-0QO03TedGlkEvaX6atGKvI",
+    authDomain: "healthguard-85677.firebaseapp.com",
+    projectId: "healthguard-85677",
+    storageBucket: "healthguard-85677.appspot.com",
+    messagingSenderId: "469620914878",
+    appId: "1:469620914878:web:92d87a7d534a831a5c3cef"
+  };
+  
+  // Initialize Firebase
+  const app = firebase.initializeApp(firebaseConfig);
