b3bf44bbd2757959569b4866ea78f7c17386dba1
KeYang <kyang72@my.bcit.ca>
2021-11-13 16:12:41 -0800

    add seperate script for main.html, move moda.html, newUnserInfor.html basicinfor.html to history file

--- baiscinfo.js
+++
@@ -1,9 +0,0 @@
-function showImg(){
-    const input = document.querySelector("#mypic-input");
-    const profile = document.querySelector("#mypic-goes-here");
-
-    input.addEventListener('change', function(e){
-        var blob = URL.createObjectURL(e.target.files[0]);
-        profile.src = blob;
-    })
-}
