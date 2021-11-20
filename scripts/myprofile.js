0038070099e6c27dcb3a57e2a27bf1f201f878db
Ramil Garipov <garipovramil93@hotmail.ca>
2021-11-19 17:05:12 -0800

    fixed .js files relevant to home.html

--- scripts/myprofile.js
+++ scripts/myprofile.js
@@ -1,3 +1,29 @@
+$(document).ready(function () {
+
+  firebase.auth().onAuthStateChanged((user) => {
+    // Check if user is signed in:
+    if (user) {
+      // Do something for the current logged-in user here:
+      console.log("Checking if user is new:");
+      //go to the correct user document by referencing to the user uid
+      currentUser = db.collection("users").doc(user.uid);
+     
+      //get the document for current user.
+      currentUser.get().then((userDoc) => {
+        //get user information from document and stored in variables .
+
+        var user_gender = userDoc.data().gender;
+        // check if documents have that variables, assign the value to span with certain id. 
+        // console.log(user_gender);
+        console.log("Click the Update Basic Information button if you would like to make changes.");
+      });
+    } else {
+      // No user is signed in.
+    }
+  });
+
+});
+
 //This code displays current user information in the forms on the page.
 function getBasicInfor() {
   firebase.auth().onAuthStateChanged((user) => {
