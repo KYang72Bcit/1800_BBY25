2f90b039f7af2d084db97dca0cb7750da5ef1b86
KeYang <kyang72@my.bcit.ca>
2021-11-23 22:11:24 -0800

    working for myscreening and home page

--- scripts/home.js
+++ scripts/home.js
@@ -7,12 +7,49 @@ $(document).ready(function () {
         console.log("Checking if user is new:");
         //go to the correct user document by referencing to the user uid
         currentUser = db.collection("users").doc(user.uid);
+        
        
         //get the document for current user.
         currentUser.get().then((userDoc) => {
           //get user information from document and stored in variables .
+          var screeninglist = userDoc.data().screeninglist;
+          if(screeninglist != null){
+            currentScreen = currentUser.collection("screenings");
+           
+            const upcomingScreenings = document.getElementById("upcomingScreenings");
+            const checking = document.querySelector('#checking_go_here');
+            let ListTemplate = document.getElementById("ListTemplate");
+            upcomingScreenings.innerHTML = "<h2>Upcoming Screenings: </h2>";
+            screeninglist.forEach(async screen=>{
+              var newcard = ListTemplate.content.cloneNode(true);
+              newcard.querySelector('.screening').innerHTML = screen;
+              const screeningPackage = currentScreen.doc(screen);
+              async function getTime(){
+                
+                return screeningPackage.get().then((screeningTime)=> {
+                  
+                  const time = screeningTime.data().date;
+  
+                  return time;
+                  
+                  
+                })
+              }
+              
+              const time = await getTime();
+              
+              newcard.querySelector('.date').innerHTML = time;
+              checking.appendChild(newcard);
 
+            })
+
+
+          }
+          
           var user_gender = userDoc.data().gender;
+          
+          
+          
           // check if documents have that variables, assign the value to span with certain id. 
           console.log(user_gender);
           if (user_gender == null) {
@@ -20,7 +57,11 @@ $(document).ready(function () {
             $("#myModal").modal('show');
             console.log("Hi there, please fill out this form.");
           }
+          if(screeninglist === null){
+            document.getElementById("getRecommendations").innerHTML = "<h2>Get Recommended Screenings: </h2> <br><button class='btn btn-primary' id='gotoRecommended'>Let's go!</button>";
+          } 
         });
+
       } else {
         // No user is signed in.
       }
--- scripts/myscreenings.js
+++ scripts/myscreenings.js
@@ -163,11 +163,13 @@ firebase.auth().onAuthStateChanged(function (user) {
                 e.preventDefault();
                 console.log("in submit data");
                 const screeningArray = getScreeningArray();
+                // const screeningMap = new Map();
                 screeningArray.forEach(test => {
                     let id = test.split(" ").join("-");
                     //let text = test;
                     let time = document.getElementById(`${id}`).value;
                     //console.log(time);
+                    // screeningMap.set(test,time);
                     currentUser.collection("screenings").doc(test).set({
                         date: time
                     }).then(function(){
@@ -177,7 +179,8 @@ firebase.auth().onAuthStateChanged(function (user) {
 
                 })
                 currentUser.update({
-                    'screeninglist': screeningArray
+                    //'screeningmap': screeningMap,
+                     'screeninglist': screeningArray
                 }).then(function () {
                     window.location.href = "home.html";
                     //console.log("add userScreening list to firebase")
